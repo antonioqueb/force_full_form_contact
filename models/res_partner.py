b0ac9c95390bae88402964a9937dd1a80c6bebe5
Antonio Queb <cardstillo@gmail.com>
2026-03-31 04:32:51 -0600

    Agregar lógica para asegurar customer_rank=1 al crear contactos desde una orden de venta.

--- models/res_partner.py
+++ models/res_partner.py
@@ -14,3 +14,13 @@ class ResPartner(models.Model):
                 "'Crear y editar...' del menú desplegable."
             ))
         return super().name_create(name)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Asegurar customer_rank=1 cuando se crea desde una orden de venta."""
+        if self.env.context.get('default_customer_rank'):
+            for vals in vals_list:
+                if not vals.get('customer_rank'):
+                    vals['customer_rank'] = self.env.context['default_customer_rank']
+        records = super().create(vals_list)
+        return records
